808b7578bb19a6ab899dcc98082e3ff8dd96612d
kuzniarg <1niebian@gmail.com>
2015-05-25 18:12:02 +0200

    Drobne poprawki

--- src/Czat/OknoPlik.java
+++ src/Czat/OknoPlik.java
@@ -8,117 +8,66 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.*;
 import javax.swing.SwingUtilities;
-import javax.swing.filechooser.*;
 
-/*
- * FileChooserDemo.java uses these files:
- *   images/Open16.gif
- *   images/Save16.gif
- */
-public class OknoPlik extends JPanel
-                             implements ActionListener {
-    static private final String newline = "\n";
-    JButton dodajPlikButton, saveButton;
+public class OknoPlik extends JPanel implements ActionListener {
+
+    JButton dodajPlikButton;
     JTextArea log;
     JFileChooser fc;
 
     public OknoPlik() {
         super(new BorderLayout());
 
-        //Create the log first, because the action listeners
-        //need to refer to it.
-        log = new JTextArea(5,20);
-        log.setMargin(new Insets(5,5,5,5));
+        log = new JTextArea(5, 20);
+        log.setMargin(new Insets(5, 5, 5, 5));
         log.setEditable(false);
         JScrollPane logScrollPane = new JScrollPane(log);
 
-        //Create a file chooser
         fc = new JFileChooser();
 
-        //Uncomment one of the following lines to try a different
-        //file selection mode.  The first allows just directories
-        //to be selected (and, at least in the Java look and feel,
-        //shown).  The second allows both files and directories
-        //to be selected.  If you leave these lines commented out,
-        //then the default mode (FILES_ONLY) will be used.
-        //
-        //fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
-        //fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-
-        //Create the open button.  We use the image from the JLF
-        //Graphics Repository (but we extracted it from the jar).
         dodajPlikButton = new JButton("Wybierz plik");
         dodajPlikButton.addActionListener(this);
 
-        //For layout purposes, put the buttons in a separate panel
-        JPanel buttonPanel = new JPanel(); //use FlowLayout
+        JPanel buttonPanel = new JPanel();
         buttonPanel.add(dodajPlikButton);
 
-        //Add the buttons and the log to this panel.
         add(buttonPanel, BorderLayout.PAGE_START);
         add(logScrollPane, BorderLayout.CENTER);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-            int returnVal = fc.showOpenDialog(OknoPlik.this);
-            if (returnVal == JFileChooser.APPROVE_OPTION) {
-                File file = fc.getSelectedFile();
-                //This is where a real application would open the file.
-                log.append("Otwieranie pliku: " + file.getName() + "." + newline);
-                try {
-                    wczytajPlik(file);
-                    log.append("Dodano do bazy.");  
-                } catch (FileNotFoundException ex) {
-                    Logger.getLogger(OknoPlik.class.getName()).log(Level.SEVERE, null, ex);
-                    log.append("Błąd podczas dodawania do bazy.");
-                } catch (IOException ex) {
-                    Logger.getLogger(OknoPlik.class.getName()).log(Level.SEVERE, null, ex);
-                    log.append("Błąd podczas dodawania do bazy.");
-                }
+        int pom = fc.showOpenDialog(OknoPlik.this);
+        if (pom == JFileChooser.APPROVE_OPTION) {
+            File plik = fc.getSelectedFile();
+            log.append("Otwieranie pliku: " + plik.getName() + ".\n");
+            try {
+                wczytajPlik(plik);
+                log.append("Dodano do bazy.");
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(OknoPlik.class.getName()).log(Level.SEVERE, null, ex);
+                log.append("Błąd podczas dodawania do bazy.");
+            } catch (IOException ex) {
+                Logger.getLogger(OknoPlik.class.getName()).log(Level.SEVERE, null, ex);
+                log.append("Błąd podczas dodawania do bazy.");
             }
-            log.setCaretPosition(log.getDocument().getLength());
-    }
-
-    /** Returns an ImageIcon, or null if the path was invalid. */
-    protected static ImageIcon createImageIcon(String path) {
-        java.net.URL imgURL = OknoPlik.class.getResource(path);
-        if (imgURL != null) {
-            return new ImageIcon(imgURL);
-        } else {
-            System.err.println("Couldn't find file: " + path);
-            return null;
         }
+        log.setCaretPosition(log.getDocument().getLength());
     }
 
-    /**
-     * Create the GUI and show it.  For thread safety,
-     * this method should be invoked from the
-     * event dispatch thread.
-     */
-    private static void createAndShowGUI() {
-        //Create and set up the window.
+    private static void initOknoPlik() {
         JFrame frame = new JFrame("Dodaj plik");
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-
-        //Add content to the window.
         frame.add(new OknoPlik());
-
-        //Display the window.
         frame.pack();
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
     }
 
-    public static void main(String[] args) {
-        //Schedule a job for the event dispatch thread:
-        //creating and showing this application's GUI.
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                //Turn off metal's use of bold fonts
-                UIManager.put("swing.boldMetal", Boolean.FALSE); 
-                createAndShowGUI();
-            }
+    public static void OknoPlikStart() {
+        SwingUtilities.invokeLater(() -> {
+            UIManager.put("swing.boldMetal", Boolean.FALSE);
+            initOknoPlik();
         });
     }
 }
